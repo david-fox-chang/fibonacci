46ad7fcc44ffd260b53524edbef2bd3fd34443bb
David <david.fox0904@gmail.com>
2018-12-07 18:25:29 +0800

    migrate syntax to ES6

--- fibonacci.js
+++ fibonacci.js
@@ -1,31 +1,29 @@
-const fibonacci = (function () {
+const fibonacci = (() => {
 
-  const fabcount = function ({i, a0, a1}) {
-    return { i: i + 1, a0: a1, a1: a0 + a1 };
-  };
+  const fibcount = ({i, a0, a1}) => ({ i: i + 1, a0: a1, a1: a0 + a1 });
 
   const fibonacciGen = function* () {
     let fab0 = { i: 0, a0: -1, a1: 1 }, fab;
 
     while (true) {
-      fab = fabcount(fab0);
+      fab = fibcount(fab0);
       yield fab.a1;
       fab0 = fab;
     }
   };
 
-  const fibonacci_n = function (n) {
+  const fibonacci_n = n => {
     let fab0 = { i: 0, a0: -1, a1: 1 }, fab;
 
     while (fab0.i <= n) {
-      fab = fabcount(fab0);
+      fab = fibcount(fab0);
       fab0 = fab;
     }
 
     return fab.a1;
   };
 
-  return function (n) {
+  return (n = undefined) => {
     if (n === undefined) {
       return fibonacciGen();
     }
