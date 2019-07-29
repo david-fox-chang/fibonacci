fc37b2a566c8cd6e81dfc7ab4117e09418365785
David <david.fox0904@gmail.com>
2019-07-29 12:19:58 +0800

    fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -1,34 +1,29 @@
-const fibonacci = (() => {
+const fibonacci = function* (times = undefined) {
+  const count = ({ i, a0, a1 }) => ({ i: i + 1, a0: a1, a1: a0 + a1 });
+  let fib0 = { i: 0, a0: -1, a1: 1 };
+  let fib;
+  let test;
 
-  const fibcount = ({i, a0, a1}) => ({ i: i + 1, a0: a1, a1: a0 + a1 });
-
-  const fibonacciGen = function* () {
-    let fab0 = { i: 0, a0: -1, a1: 1 }, fab;
-
-    while (true) {
-      fab = fibcount(fab0);
-      yield fab.a1;
-      fab0 = fab;
+  try {
+    if (times === undefined) {
+      console.warn('endless fibonacci sequence was created')
+      test = () => true;
+    } else if (typeof times !== 'number') {
+      throw new Error('times need a number');
+    } else if (times >= 0) {
+      test = ({ i }) => i <= times;
+    } else {
+      console.error('An Serial Number Less than 0 Was Received.');
+      console.warn('show test fibonacci sequence');
+      test = ({ i }) => i <= 5;
     }
-  };
-
-  const fibonacci_n = n => {
-    let fab0 = { i: 0, a0: -1, a1: 1 }, fab;
-
-    while (fab0.i <= n) {
-      fab = fibcount(fab0);
-      fab0 = fab;
+  } catch (e) {
+    console.error('An Incorrect Serial Number Was Received.');
+  } finally {
+    while (test(fib0)) {
+      fib = count(fib0);
+      yield fib.a1;
+      fib0 = fib;
     }
-
-    return fab.a1;
-  };
-
-  return (n = undefined) => {
-    if (n === undefined) {
-      return fibonacciGen();
-    }
-
-    return fibonacci_n(n);
-  };
-
-})();
+  }
+}
