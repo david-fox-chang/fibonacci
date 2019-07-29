bda7630d8a70737d18c990f667525332cec91e7f
David <david.fox0904@gmail.com>
2019-07-29 12:23:50 +0800

    show warn

--- fibonacci.js
+++ fibonacci.js
@@ -1,4 +1,4 @@
-const fibonacci = function* (times = undefined) {
+const fibonacci = function* (times = undefined, showwarn = false) {
   const count = ({ i, a0, a1 }) => ({ i: i + 1, a0: a1, a1: a0 + a1 });
   let fib0 = { i: 0, a0: -1, a1: 1 };
   let fib;
@@ -6,7 +6,7 @@ const fibonacci = function* (times = undefined) {
 
   try {
     if (times === undefined) {
-      console.warn('endless fibonacci sequence was created')
+      showwarn && console.warn('endless fibonacci sequence was created')
       test = () => true;
     } else if (typeof times !== 'number') {
       throw new Error('times need a number');
@@ -14,7 +14,7 @@ const fibonacci = function* (times = undefined) {
       test = ({ i }) => i <= times;
     } else {
       console.error('A Negative Number Was Received.');
-      console.warn('show test fibonacci sequence');
+      showwarn && console.warn('show test fibonacci sequence');
       test = ({ i }) => i <= 5;
     }
   } catch (e) {
